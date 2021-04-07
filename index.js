69b8fa67d23bd96ae2aedc1157b1bf3d11fc1b0d
yokesh-ashwin <71142959+yokesh-ashwin@users.noreply.github.com>
2021-04-07 21:34:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;//1-6
+
+var randomDiceImage = "Dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png-dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//if part
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🍾Player ones wins !!🍕";
+
+}
+
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML = "🍻Player two wins🥂";
+
+}
+
+else{
+    document.querySelector("h1").innerHTML = "it's draw Nibba 👽";
+}
